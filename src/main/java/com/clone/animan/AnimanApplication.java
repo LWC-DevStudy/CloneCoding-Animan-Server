0e00735f9001479713f3ff3df90446e45f0e47cc
timchae <timchae2@gmail.com>
2021-08-20 17:45:06 +0900

    Create project #2

---
+++ src/main/java/com/clone/animan/AnimanApplication.java
@@ -0,0 +1,13 @@
+package com.clone.animan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clone/animan/AnimanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.animan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
